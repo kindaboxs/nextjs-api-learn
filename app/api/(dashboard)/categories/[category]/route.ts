67df35f5d53fb5187eedddd7327405b1084590c7
mrboxs <rizkywanda200155@gmail.com>
2024-10-06 03:01:13 +0700

    functionally delete category api's

--- app/api/(dashboard)/categories/[category]/route.ts
+++ app/api/(dashboard)/categories/[category]/route.ts
@@ -52,7 +52,10 @@ export const PATCH = async (req: NextRequest, context: Context) => {
 
     if (!findCategory) {
       return new NextResponse(
-        JSON.stringify({ message: "Category not found in the database" }),
+        JSON.stringify({
+          message:
+            "Category not found in the database or not belong to the user",
+        }),
         { status: 404 }
       );
     }
@@ -74,6 +77,70 @@ export const PATCH = async (req: NextRequest, context: Context) => {
     );
   } catch (error) {
     return new NextResponse(`Error in updating category: ${error}`, {
+      status: 500,
+    });
+  }
+};
+
+export const DELETE = async (req: NextRequest, context: Context) => {
+  const categoryId = context.params.category;
+
+  try {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing userId" }),
+        { status: 400 }
+      );
+    }
+
+    if (!categoryId) {
+      return new NextResponse(
+        JSON.stringify({ message: "Category not found" }),
+        { status: 404 }
+      );
+    }
+
+    const user = await getUserById(userId);
+
+    if (!user) {
+      return new NextResponse(
+        JSON.stringify({ message: "User not found in the database" }),
+        { status: 404 }
+      );
+    }
+
+    const findCategory = await db.category.findFirst({
+      where: {
+        id: categoryId,
+        userId: userId,
+      },
+    });
+
+    if (!findCategory) {
+      return new NextResponse(
+        JSON.stringify({
+          message:
+            "Category not found in the database or not belong to the user",
+        }),
+        { status: 404 }
+      );
+    }
+
+    await db.category.delete({
+      where: { id: categoryId },
+    });
+
+    return new NextResponse(
+      JSON.stringify({
+        message: "Category has been deleted!",
+      }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new NextResponse(`Error in deleting category: ${error}`, {
       status: 500,
     });
   }
